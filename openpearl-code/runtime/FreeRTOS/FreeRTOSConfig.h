dc4cf37c2067104ec77bc4cf5499f46588e1e9b4
Jonas Meyer <quitte@gmail.com>
2015-05-28 23:48:54 +0200

    FreeRTOS: cleaned up config

--- openpearl-code/runtime/FreeRTOS/FreeRTOSConfig.h
+++ openpearl-code/runtime/FreeRTOS/FreeRTOSConfig.h
@@ -99,10 +99,8 @@
 #define configUSE_MUTEXES			1
 
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION   0 /* added */
-//#define configUSE_TIMERS		1	/* added */
 #define INCLUDE_xTaskGetSchedulerState  1	/* added */
 #define INCLUDE_xTaskResumeFromISR	1	/* added */
-#define configUSE_APPLICATION_TASK_TAG	1	/* added */
 #define configTIMER_TASK_PRIORITY       (configMAX_PRIORITIES - 1) 
 #define configTIMER_QUEUE_LENGTH  50
 #define configTIMER_TASK_STACK_DEPTH	300
