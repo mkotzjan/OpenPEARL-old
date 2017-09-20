b044ae1aa1deb3aa9ddccc7e2ce9bf1af14cbc4f
Rainer Mueller (debian8) <mueller@hs-furtwangen.de>
2017-09-20 17:36:51 +0200

    autoconf.h include added and conditional compilations for CONFIG_HAS_FAT

--- openpearl-code/runtime/FreeRTOS/addOns/mkFakeTypes.c
+++ openpearl-code/runtime/FreeRTOS/addOns/mkFakeTypes.c
@@ -1,13 +1,21 @@
+#include "../../configuration/include/autoconf.h"
+
 #include "FreeRTOSConfig.h"
 #include "FreeRTOS.h"
 #include "task.h"
+
+
+// FreeRTOS types
+TCB_t __attribute__((section ("TCB_T"))) tcb;
+char __attribute__ ((section ("STACKTYPE"))) stack[sizeof(portSTACK_TYPE)];
+char __attribute__ ((section ("PORTSHORT"))) shortT[sizeof(portSHORT)];
+char __attribute__ ((section ("UBASETYPE"))) ubasetype[sizeof(UBaseType_t)];
+
+// FatFs types
+#ifdef CONFIG_HAS_FAT 
 #include "ff.h"
 
-TCB_t __attribute__((section ("TCB_T"))) tcb;
-char __attribute__ ((section ("STACKTYPE"))) stack[sizeof(portSTACK_TYPE)];;
-char __attribute__ ((section ("PORTSHORT"))) shortT[sizeof(portSHORT)];;
-char __attribute__ ((section ("UBASETYPE"))) ubasetype[sizeof(UBaseType_t)];;
-char __attribute__ ((section ("FATFS"))) fatfs[sizeof(FATFS)];;
-char __attribute__ ((section ("FIL"))) fatFil[sizeof(FIL)];;
-char __attribute__ ((section ("VOLUMES"))) fatVol[sizeof(_VOLUMES)];;
-
+char __attribute__ ((section ("FATFS"))) fatfs[sizeof(FATFS)];
+char __attribute__ ((section ("FIL"))) fatFil[sizeof(FIL)];
+char __attribute__ ((section ("VOLUMES"))) fatVol[sizeof(_VOLUMES)];
+#endif
