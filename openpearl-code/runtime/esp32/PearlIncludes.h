205176a8645c53d5f9d1e505863bbb0e76cc26d0
Michael Kotzjan <mich3ael@web.de>
2018-06-18 11:22:48 +0200

    Added TCP/IP Server to Makefile and fixed absolute paths

--- openpearl-code/runtime/esp32/PearlIncludes.h
+++ openpearl-code/runtime/esp32/PearlIncludes.h
@@ -101,8 +101,8 @@ Only this file mus be included.
 #include "Array.h"
 
 #include "Esp32Wifi.h"
+#include "TcpIpServer.h"
 
 //#include "Esp32BME280.h"
 //#include "I2CBus.h"
-//#include "TcpIpServer.h"
 
